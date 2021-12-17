ada645517c2cf81873ea7eeaaafa88d39082f347
Eirfire <77374574+Eirfire@users.noreply.github.com>
2021-12-18 07:44:36 +1000

    Update Test addon.py

--- Test addon.py
+++ Test addon.py
@@ -10,6 +10,7 @@ bl_info = {
     "tracker_url": "",
     "category": "Object",
 }
+from typing import Text
 import bpy
 
 
@@ -53,32 +54,51 @@ class MESH_OT_MONKEY_grid(bpy.types.Operator):
         return {'FINISHED'}
     
 
-class VEIW3D_PT_monkey_grid(bpy.types.Panel):
+
+
+class TestPanel(bpy.types.Panel):
+    bl_label = "test Panel"
+    bl_idname = "PT_TestPanel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'Monkeys'
-    bl_label = 'Grid'
-    
+    bl_category = 'My 1st Addon'
+
     def draw(self, context):
-        self.layout.operator('mesh.monkey_grid',
-             text= 'Default grid',
-             icon= 'MONKEY')
+        layout = self.layout
+
+        row = layout.row()
+        row.label(text="Add object", icon= "CUBE")
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add", icon="CUBE")
+        row = layout.row()
+        row.operator("mesh.primitive_uv_sphere_add", icon="MESH_UVSPHERE")
+        row = layout.row()
+        row.operator("mesh.monkey_grid", icon="MONKEY")
         
     
     
     
 def register():
     bpy.utils.register_class(MESH_OT_MONKEY_grid)
-    bpy.utils.register_class(VEIW3D_PT_monkey_grid)
+    bpy.utils.register_class(TestPanel)
+   
     
 def unregister():
     bpy.utils.unregister_class(MESH_OT_MONKEY_grid)
-    bpy.utils.unregister_class(VEIW3D_PT_monkey_grid)
-
-if __name__ == '__main__':
-	register()
+    bpy.utils.unregister_class(TestPanel)
     
+if __name__ == '__main__':
+    register()
 
+
+"""class VIEW3D_PT_monkey_grid(bpy.types.Panel):
+    bl_space_type = 'VIEW3D'
+    bl_region_type = 'UI'
+    bl_category = "Monkeys"
+    bl_label = "Grid"
+
+    def draw(self, context):
+        self.layout.operator('mesh.monkey_grid', text= "default grid", icon="MONKEY")"""
       
       
       
