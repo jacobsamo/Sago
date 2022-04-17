eaaaaf1f39588648d7881f8f514642da4f5f1fb6
Eirfire <jacob35422@gmail.com>
2022-04-17 10:34:51 +1000

    Updated files

--- Sago.py
+++ Sago.py
@@ -28,20 +28,20 @@ bl_info = {
     "tracker_url": "https://github.com/Eirfire/Blender-addon/issues",
     "support": "COMMUNITY",
     "category": "Generic",
-    }
+}
 
 
 #import blender python modules 
-from ast import Pass
-from typing import Text
+from unicodedata import name
 import bpy
+import bmesh
 from bpy.props import (StringProperty, BoolProperty, IntProperty, FloatProperty, FloatVectorProperty, EnumProperty, PointerProperty,)
 from bpy.types import (Panel, Menu, Operator, PropertyGroup,)
 #import other python modules 
 import os
 import time
-import random
-
+import math
+from math import *
 
 
 
@@ -84,10 +84,6 @@ class VEIW3D_PT_ExtraRender(Panel):
     bl_category = "Sago"
 
 
-    @classmethod
-    def poll(self,context):
-        return context.object is not None
-
     def draw(self, context):
         layout = self.layout
         scene = context.scene
@@ -112,14 +108,14 @@ class VEIW3D_PT_ExtraRender(Panel):
         
 def some_other_function(dummy):
     print("Render complete")
-    bpy.context.scene.my_tool.close_blender = False
+    bpy.types.scene.sago.close_blender = False
     bpy.ops.wm.save_mainfile()
     time.sleep(2)
     bpy.ops.wm.quit_blender()    
 
 def some_function(dummy):
     print("Render complete")
-    bpy.context.scene.my_tool.shutdown_computer = False
+    bpy.types.scene.sago.shutdown_computer = False
     bpy.ops.wm.save_mainfile()
     time.sleep(2)
     os.system("shutdown /s /t 1")
@@ -322,7 +318,207 @@ class NODE_OT_customgroup(Operator):
             
         return {"FINISHED"}
 
+class SAGO_OT_add_displace(Operator):
+    bl_idname = "sago.add_displacement"
+    bl_label = "Displace"
+    bl_options = {"REGISTER", "UNDO"}
 
+    texture_type: EnumProperty(
+        name= "", 
+        description= "Select things",
+        items= [
+            ('1', "Clouds", "Clouds Texture"),
+            ('2', "Vorinoi", "Clouds Texture")
+        ]
+    )
+    smooth: BoolProperty(name="Smooth", description= "Use auto smooth", default=False )
+    shade_smooth: BoolProperty(name="Shade Smooth", description= "Shade smooth", default=False )
+    deg : FloatProperty(
+		name        = "Angle",
+		description = "Auto smooth angle",
+		default     = radians(30),
+		min         = 0,
+		max         = radians(180),
+		step        = 10,
+		precision   = 3,
+		subtype     = "ANGLE"
+		)
+    sub_type: EnumProperty(
+        name= "",
+        description= "Selct things",
+        items= [ 
+            ('op1', "Simple", "Use simple subsurf type"),
+            ('op2', "Catmull-Clark", "Use Catmull-Clark subsurf type"),
+            
+            
+        ]
+    )
+    subdive_amount: IntProperty(
+        name="subdive",
+        description="Subdivide amount",
+        default =1, 
+        min = 0, soft_max = 10
+    )
+    strength: FloatProperty(
+        name = "strength",
+        description="displace strength",
+        default = 1, 
+        soft_min = 0, max = 100
+    )
+    mid_level: FloatProperty(
+        name = "mid level",
+        description="displace mid level",
+        default = 0.5, 
+        min = 0, max = 1
+    )
+    vo_noise_intensity: FloatProperty(
+        name = "Noise Intensity",
+        description="Texture intensity",
+        default = 1, 
+        min = 0.01, max = 10
+    )
+    tex_noise_scale: FloatProperty(
+        name = "Noise scale",
+        description="Texture Scale",
+        default = 0.25, 
+        min = 0, soft_max = 2
+    )
+    colu_noise_depth: IntProperty(
+        name = "Noise Depth",
+        description="Texture depth",
+        default = 2, 
+        min = 0, soft_max = 24
+    )
+    tex_noise_nabla: FloatProperty(
+        name = "Noise nabla",
+        description="Texture nabla",
+        default = 0.03, 
+        min = 0, max = 0.1
+    )
+    use_sub: BoolProperty(name="SubSurf", description= "Use Sub Surf", default=False)
+    sub_amount: IntProperty(name="", description="", default=2, min=0, max=11)
+    
+    submesh: BoolProperty(name="Subdivide", description="Subdive Mesh", default=False)
+    
+
+    def execute(self, context):
+        obj = bpy.context.active_object
+        lev = self.sub_amount
+        angle = self.deg
+        strength = self.strength
+        mid_level = self.mid_level
+        subdiv_amount = self.subdive_amount
+        noise_scale = self.tex_noise_scale
+        noise_depth = self.colu_noise_depth
+        noise_nabla = self.tex_noise_nabla
+        
+
+        #to create a modifer
+        #obj.modifiers.new("Name of modifer e.g: sago displace", "Type of modifer e.g SUBSURF")
+        bpy.ops.object.editmode_toggle()
+        if self.submesh == True:
+            bpy.ops.mesh.subdivide(number_cuts=subdiv_amount)
+        else:
+            bpy.ops.mesh.subdivide(number_cuts=0)
+        bpy.ops.object.editmode_toggle()
+
+        #create subsurf modifer if use_sub is True
+        if self.use_sub == True:
+            sub = obj.modifiers.new("Sago Subdiv", 'SUBSURF')
+            if self.sub_type == 'op1':
+                sub.subdivision_type = 'SIMPLE'
+            if self.sub_type == 'op2':
+                sub.subdivision_type = 'CATMULL_CLARK'
+            sub.levels = lev
+            sub.render_levels = lev
+
+        #create texture
+        if self.texture_type == "1":
+            #check if texture exists if not creates a new texture
+            if bpy.data.textures.get("Sago Displace Texture") != None: 
+                print("Texture exists")
+            else:
+                bpy.data.textures.new("Sago Displace Texture", 'CLOUDS')
+
+            new_tex = bpy.data.textures['Sago Displace Texture']
+            new_tex.type = 'CLOUDS'
+            new_tex.noise_depth = noise_depth
+            new_tex.noise_scale = noise_scale
+            new_tex.nabla = noise_nabla
+        if self.texture_type == "2":
+            if bpy.data.textures.get("Sago Displace Texture") != None: 
+                print("Texture exists")
+            else:
+                bpy.data.textures.new("Sago Displace Texture", 'VORONOI')
+            new_tex = bpy.data.textures['Sago Displace Texture']
+            new_tex.type = 'VORONOI'
+            new_tex.noise_intensity = self.vo_noise_intensity
+            new_tex.noise_scale = noise_scale
+            new_tex.nabla = noise_nabla
+        
+        #create a displace modifier
+        dis = obj.modifiers.new("Sago Dsiplace", 'DISPLACE')
+        dis.texture = new_tex
+        dis.strength = strength
+        dis.mid_level = mid_level
+
+        
+
+        if self.shade_smooth == True:
+            bpy.ops.object.shade_smooth()
+        
+
+        
+        if self.smooth == True:
+            mesh = obj.data
+            mesh.use_auto_smooth = True
+            mesh.auto_smooth_angle = angle
+            
+        return {'FINISHED'}
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = False
+        layout.use_property_decorate = True  # No animation.
+
+
+        layout.separator(factor= 0.1)
+        col = layout.column(align=True)
+        col.label(text="Displace stregth and mid level")
+        col.prop(self, "strength", text="Displace strength")
+        col.prop(self, "mid_level", text="displace mid level")
+        col.separator()
+        col.label(text="Texture Settings:")
+        col.prop(self, "texture_type", text="Texture type")
+        if self.texture_type == "1":
+            col.prop(self, "tex_noise_scale")
+            col.prop(self, "colu_noise_depth")
+            col.prop(self, "tex_noise_nabla")
+        if self.texture_type == "2":
+            col.prop(self, "vo_noise_intensity")
+            col.prop(self, "tex_noise_scale")
+            col.prop(self, "tex_noise_nabla")
+
+        flow = col.column_flow(columns=2, align=True)
+        flow.prop(self, "submesh")
+        if self.submesh == True:
+            col.prop(self, "subdive_amount", text="Sudvide amount", icon="MESH_GRID")
+    
+
+        
+        flow.prop(self, "use_sub")
+        if self.use_sub == True:
+            col.prop(self, "sub_type", text="Subsurf type")
+            col.prop(self, "sub_amount", text="Subsurf amount") 
+        col.separator()
+        flow.prop(self, "smooth", text="Use Auto smooth")
+        flow.prop(self, "shade_smooth")
+        if self.smooth == True:
+            col.prop(self, "deg", slider= True)
+        
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self)
     
 
 
@@ -348,7 +544,8 @@ class WM_MT_pie_menu(Menu):
             pie.operator("mesh.quads_convert_to_tris", icon="IPO_LINEAR")
             pie.operator("mesh.select_random", icon="MOD_ARRAY")
 
-
+        if mode == "OBJECT":
+            pie.operator("sago.add_displacement", icon="MOD_DISPLACE")
         #Any Mode
         pie.operator("sago.camera_settings", icon="VIEW_CAMERA")
         pie.operator("mesh.monkey_grid", icon="MONKEY")
@@ -503,6 +700,7 @@ SagoProperties,
 MESH_OT_MONKEY_grid,
 camera_settings,
 NODE_OT_customgroup,
+SAGO_OT_add_displace,
 )
 
 # Register
